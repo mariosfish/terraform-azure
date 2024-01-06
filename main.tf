f0f3dc87fec5bf607ccd9b3767609c3f8d874967
Marios Michalopoulos <mariosfish1@gmail.com>
2024-01-06 22:46:14 +0200

    Add custom data to vm to install docker on creation.

--- main.tf
+++ main.tf
@@ -105,6 +105,8 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
     azurerm_network_interface.mtc-nic.id,
   ]
 
+custom_data = filebase64("customdata.tpl")
+
   admin_ssh_key {
     username   = "adminuser"
     public_key = file("~/.ssh/tf-azure-key.pub")
@@ -126,3 +128,4 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
     environment = "dev"
   }
 }
+
